7178d28c917923360f4fa91addf0b59e7eb361a5
komekami <komekami0131@gmail.com>
2024-06-19 07:15:26 +0000

    feat: add nvim-treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -36,6 +36,15 @@ return require('packer').startup(function(use)
     'hrsh7th/nvim-cmp',
   }
 
+  -- Syntax highlight
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+        ts_update()
+    end,
+  }
+
   use {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
